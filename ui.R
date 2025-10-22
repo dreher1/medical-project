4c2d061bc1dd41cac5fdc6446c6a22b640bdd2f9
Reher <dreher@mail.wlu.edu>
2025-10-22 16:28:55 -0400

    fix

--- ui.R
+++ ui.R
@@ -97,7 +97,7 @@ shinyUI(
         p("Explore the melanoma_table interactively:"),
         reactableOutput("data_table", height = 500),
         br(),
-        textOutput("data_summary")
+        textOutput("data_summary") 
       )
     )
   )
