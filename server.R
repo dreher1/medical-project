d4eea9f1e061a1d535c2a5b547db1afcf3004fde
Patrick Murphy <pmurphy@mail.wlu.edu>
2025-10-27 15:28:23 -0400

    made code cleaner

--- server.R
+++ server.R
@@ -37,25 +37,105 @@ shinyServer(function(input, output, session) {
     )
   })
   
+  # HELPER FUNCTION: Draw melanoma choropleth (reusable!)
+  draw_melanoma <- function(state_name) {
+    proxy <- leafletProxy("map")
+    
+    # Clear old melanoma layer
+    proxy %>% clearGroup("melanoma") %>% removeControl("melanoma_legend")
+    
+    # Only draw if checkbox is checked AND state is selected
+    if (!("Melanoma by County" %in% input$viz_options)) {
+      return(invisible())
+    }
+    
+    if (state_name == "All states (USA)") {
+      showNotification(
+        "Please select a specific state to view melanoma data",
+        type = "warning",
+        duration = 3
+      )
+      return(invisible())
+    }
+    
+    # Get state abbreviation
+    state_abbr <- state.abb[match(state_name, state.name)]
+    if (is.na(state_abbr) && state_name == "District of Columbia") {
+      state_abbr <- "DC"
+    }
+    
+    # Filter and join data
+    state_counties <- counties_sf[counties_sf$STUSPS == state_abbr, ]
+    counties_with_data <- state_counties %>%
+      left_join(
+        melanoma_table[, c("fips_melanoma", "county", "state", "avg_annual_ct")],
+        by = c("GEOID" = "fips_melanoma")
+      )
+    
+    counties_with_data <- counties_with_data[!is.na(counties_with_data$avg_annual_ct), ]
+    
+    if (nrow(counties_with_data) == 0) {
+      return(invisible())
+    }
+    
+    # Create color palette
+    pal <- colorBin(
+      palette = "YlOrRd",
+      domain = counties_with_data$avg_annual_ct,
+      bins = c(0, 10, 25, 50, 100, 250, 500, Inf),
+      na.color = "#808080"
+    )
+    
+    # Draw the choropleth
+    proxy %>% 
+      addPolygons(
+        data = counties_with_data,
+        fillColor = ~pal(avg_annual_ct),
+        weight = 1,
+        opacity = 1,
+        color = "white",
+        fillOpacity = 0.7,
+        group = "melanoma",
+        label = ~paste0(NAME, " County: ", avg_annual_ct, " cases/year"),
+        highlightOptions = highlightOptions(
+          weight = 2,
+          color = "#666",
+          fillOpacity = 0.9,
+          bringToFront = TRUE
+        )
+      ) %>%
+      addLegend(
+        position = "bottomright",
+        pal = pal,
+        values = counties_with_data$avg_annual_ct,
+        title = "Annual<br>Melanoma Cases<br>(County Level)",
+        opacity = 0.7,
+        layerId = "melanoma_legend"
+      )
+  }
+  
+  # Watch state dropdown
   observeEvent(input$state_select, {
     req(input$state_select)
     
     proxy <- leafletProxy("map")
     
-    #I think code below clears when not selected 
+    # Clear everything when state changes
     proxy %>% 
       clearGroup("state_focus") %>% 
       clearGroup("melanoma") %>% 
       removeControl("melanoma_legend")
     
+    # Handle "All states" selection
     if (input$state_select == "All states (USA)") {
       proxy %>% flyTo(lng = -98.5, lat = 39.8, zoom = 4)
       return(invisible())
     }
     
+    # Draw state outline
     sel <- states_sf[states_sf$NAME == input$state_select, ]
     if (nrow(sel) < 1) {
-      showNotification("State not found in shapes. Check the name.", type = "warning")
+      showNotification("State not found", type = "warning")
       return(invisible())
     }
     
@@ -69,143 +149,21 @@ shinyServer(function(input, output, session) {
         opacity = 1,
         group = "state_focus"
       ) %>%
-      fitBounds(lng1 = bb[["xmin"]], lat1 = bb[["ymin"]], #Zooms to correct state
+      fitBounds(lng1 = bb[["xmin"]], lat1 = bb[["ymin"]],
                 lng2 = bb[["xmax"]], lat2 = bb[["ymax"]])
     
-    
-    if ("Melanoma by County" %in% input$viz_options) {
-      
-      state_abbr <- state.abb[match(input$state_select, state.name)]
-      if (is.na(state_abbr) && input$state_select == "District of Columbia") {
-        state_abbr <- "DC"
-      }
-      
-      state_counties <- counties_sf[counties_sf$STUSPS == state_abbr, ]
-      
-      counties_with_data <- state_counties %>%
-        left_join(
-          melanoma_table[, c("fips_melanoma", "county", "state", "avg_annual_ct")],
-          by = c("GEOID" = "fips_melanoma")
-        )
-      
-      counties_with_data <- counties_with_data[!is.na(counties_with_data$avg_annual_ct), ]
-      
-      if (nrow(counties_with_data) > 0) {
-        pal <- colorBin(
-          palette = "YlOrRd",
-          domain = counties_with_data$avg_annual_ct,
-          bins = c(0, 10, 25, 50, 100, 250, 500, Inf),
-          na.color = "#808080"
-        )
-        
-        proxy %>% 
-          addPolygons(
-            data = counties_with_data,
-            fillColor = ~pal(avg_annual_ct),
-            weight = 1,
-            opacity = 1,
-            color = "white",
-            fillOpacity = 0.7,
-            group = "melanoma",
-            label = ~paste0(NAME, " County: ", avg_annual_ct, " cases/year"),
-            highlightOptions = highlightOptions(
-              weight = 2,
-              color = "#665",
-              fillOpacity = 0.9,
-              bringToFront = TRUE
-            )
-          ) %>%
-          addLegend(
-            position = "bottomright",
-            pal = pal,
-            values = counties_with_data$avg_annual_ct,
-            title = "Annual<br>Melanoma Cases<br>(County Level)",
-            opacity = 0.7,
-            layerId = "melanoma_legend"
-          )
-      }
-    }
+    # Draw melanoma if checkbox is checked
+    draw_melanoma(input$state_select)
     
   }, ignoreInit = TRUE)
   
-  
-  
-  
-  #Watches the checkbox group for changes
-  #Runs whenever user checks/unchecks ANY visualization option
-  #Is code below not same as above why need both?
+  # Watch checkbox - just call the helper function!
   observeEvent(input$viz_options, {
-    proxy <- leafletProxy("map")
-    
-    proxy %>% clearGroup("melanoma") %>% removeControl("melanoma_legend")
-    
-    if ("Melanoma by County" %in% input$viz_options) {
-      
-      if (input$state_select == "All states (USA)") {
-        showNotification(
-          "Please select a specific state to view melanoma data (prevents server overload)", 
-          type = "warning",
-          duration = 3
-        )
-        return(invisible())
-      }
-      
-      state_abbr <- state.abb[match(input$state_select, state.name)]
-      if (is.na(state_abbr) && input$state_select == "District of Columbia") {
-        state_abbr <- "DC"
-      }
-      
-      state_counties <- counties_sf[counties_sf$STUSPS == state_abbr, ]
-      
-      counties_with_data <- state_counties %>%
-        left_join(
-          melanoma_table[, c("fips_melanoma", "county", "state", "avg_annual_ct")],
-          by = c("GEOID" = "fips_melanoma")
-        )
-      
-      counties_with_data <- counties_with_data[!is.na(counties_with_data$avg_annual_ct), ]
-      
-      if (nrow(counties_with_data) == 0) {
-        showNotification("No melanoma data available for this state", type = "warning")
-        return(invisible())
-      }
-      
-      pal <- colorBin(
-        palette = "YlOrRd",
-        domain = counties_with_data$avg_annual_ct,
-        bins = c(0, 10, 25, 50, 100, 250, 500, Inf),
-        na.color = "#808080"
-      )
-      
-      proxy %>% 
-        addPolygons(
-          data = counties_with_data,
-          fillColor = ~pal(avg_annual_ct),
-          weight = 1,
-          opacity = 1,
-          color = "white",
-          fillOpacity = 0.7,
-          group = "melanoma",
-          label = ~paste0(NAME, " County: ", avg_annual_ct, " cases/year"),
-          highlightOptions = highlightOptions(
-            weight = 2,
-            color = "#665",
-            fillOpacity = 0.9,
-            bringToFront = TRUE
-          )
-        ) %>%
-        addLegend(
-          position = "bottomright",
-          pal = pal,
-          values = counties_with_data$avg_annual_ct,
-          title = "Annual<br>Melanoma Cases<br>(County Level)",
-          opacity = 0.7,
-          layerId = "melanoma_legend"
-        )
-    }
-    
+    req(input$state_select)
+    draw_melanoma(input$state_select)
   }, ignoreInit = TRUE)
   
+  # Data Explorer
   output$data_table <- renderReactable({
     df <- get0("melanoma_table", inherits = TRUE)
     req(!is.null(df))
